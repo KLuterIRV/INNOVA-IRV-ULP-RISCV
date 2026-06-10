8cbca52b6c75111cad97553c9dacf48e881d4fc7
Kristian Thomas Luter Arregui <kristian.luter@irvmic.com>
2026-06-10 09:39:51 +0200

    Test single GF180 SRAM macro with direct top-level instance

--- src/tt_um_kluterirv_rv32e_core.v
+++ src/tt_um_kluterirv_rv32e_core.v
@@ -23,14 +23,19 @@ module tt_um_kluterirv_rv32e_core (
     assign sram_addr  = {uio_in[5:1], ui_in[1]};
     assign sram_wdata = {ui_in[7:2], uio_in[1:0]};
 
-    sram_macro_smoke_test u_sram_test (
-        .clk   (clk),
-        .rst   (rst),
-        .we    (sram_we),
-        .addr  (sram_addr),
-        .wdata (sram_wdata),
-        .rdata (sram_rdata)
-    );
+    assign cen  = 1'b0;               // active-low chip enable
+    assign gwen = ~sram_we;           // active-low global write enable
+    assign wen  = sram_we ? 8'h00 : 8'hFF;
+
+gf180mcu_fd_ip_sram__sram64x8m8wm1 u_sram (
+    .CLK  (clk),
+    .CEN  (cen),
+    .GWEN (gwen),
+    .WEN  (wen),
+    .A    (sram_addr),
+    .D    (sram_wdata),
+    .Q    (sram_rdata)
+);
 
     assign uo_out = ena ? sram_rdata : 8'd0;
 
